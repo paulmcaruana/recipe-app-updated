92ccbd61302ec7494816a9af39cef2841e6e7d74
Paul <paul@beambient.co.uk>
2021-05-28 16:07:43 +0100

    http requests added

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,48 @@
+import { Injectable, Inject } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { RecipeService } from "../recipes/recipe.service";
+import { Recipe } from "../recipes/recipe.model";
+import { map, tap } from "rxjs/operators";
+
+@Injectable({ providedIn: "root" })
+export class DataStorageService {
+  constructor(
+    @Inject(HttpClient) private http: HttpClient,
+    @Inject(RecipeService) private recipeService: RecipeService
+  ) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http
+      .put(
+        "https://ng-course-recipe-book-4d7d1-default-rtdb.firebaseio.com/recipes.json",
+        recipes
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
+
+  fetchRecipes() {
+    return this.http
+      .get<Recipe[]>(
+        "https://ng-course-recipe-book-4d7d1-default-rtdb.firebaseio.com/recipes.json"
+      )
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        }),
+        tap((recipes) => {
+          this.recipeService.setRecipes(recipes);
+        })
+      )
+      .subscribe((recipes) => {
+        this.recipeService.setRecipes(recipes);
+      });
+  }
+}
